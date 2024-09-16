69c2a50b78f2ff6917c68c43cf1be43fa28ec74b
Chabardes <pierre@chabardes.net>
2024-09-16 16:43:46 +0200

    fix hand size for fist character

--- src/domain/fist/class.ts
+++ src/domain/fist/class.ts
@@ -3,7 +3,7 @@ import { fistCards } from './cards';
 
 export const fist = {
   name: FrosthavenClassNames.Fist,
-  handSize: 11,
+  handSize: 8,
   path: '/fist/icons/fh-fist-color-icon.png',
   iconSize: { width: 60, height: 60 },
   cards: fistCards,
