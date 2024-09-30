9b4b78160f4206928d7dd0ec4d53b289fca6a59e
Pierre Chabardes <pierre@chabardes.net>
2024-09-30 10:17:19 +0200

    feat(card): smoother card browsing

--- src/app/_components/cards/CardPile.tsx
+++ src/app/_components/cards/CardPile.tsx
@@ -1,6 +1,6 @@
 import { Card } from '@/domain/cards.type';
 import { CardComponent } from './Card';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { WheelAction } from '../ActionWheel';
 import { motion } from 'framer-motion';
 
@@ -15,19 +15,27 @@ export default function CardPile<X extends Card>({
   vertical?: boolean;
   name?: string;
 }) {
-  const [hoveredCardIndex, setHoveredCardIndex] = useState<number | undefined>(undefined);
+  const [hoveredCardIndex, setHoveredCardIndex] = useState<number>(cards.length - 1);
   const overlap = vertical ? '-mb-36' : '-mr-24';
+  function getZIndex(index: number) {
+    return 200 - Math.abs(((hoveredCardIndex ?? 0) - index) * 10);
+  }
+
+  useEffect(() => {
+    setHoveredCardIndex(cards.length - 1);
+  }, [cards.length]);
 
   return <div
     className={`flex flex-wrap ${vertical ? 'flex-col' : ''} gap-4 min-h-48 min-w-36`}
-    onMouseLeave={() => setHoveredCardIndex(undefined)}
+    onMouseLeave={() => setHoveredCardIndex(cards.length - 1)}
   >
     {cards
       .map((card, index) => <motion.div
         key={card.name}
         onMouseEnter={() => setHoveredCardIndex(index)}
         whileHover={{ scale: 1.1 }}
-        className={`w-fit ${hoveredCardIndex === index ? '' : overlap}`}
+        className={`w-fit ${overlap}`}
+        style={{ zIndex: getZIndex(index) }}
       >
         <CardComponent
           name={name ? `${name}-${card.name.replaceAll(' ', '-')}` : card.name}
