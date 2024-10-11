17999fa203253d45bc0b85bf9ab8f9913fc085e8
Pierre Chabardes <pierre@chabardes.net>
2024-10-11 11:45:24 +0200

    feat(metrics): install speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <main>
           {children}
+          <SpeedInsights />
         </main>
       </body>
     </html>
