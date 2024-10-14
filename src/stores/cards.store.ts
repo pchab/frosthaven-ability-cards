8360f89edfa5f76e1bfbf2a909a3af9d8828c9bc
Pierre Chabardes <pierre@chabardes.net>
2024-10-14 14:49:00 +0200

    feat(store): reorganize store types

--- src/stores/cards.store.ts
+++ src/stores/cards.store.ts
@@ -5,39 +5,46 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 import { indexedDBStorage } from './indexed-db.storage';
 
-interface AbilityCardsState {
+type AbilityCardsState = {
   level: number;
-  setLevel: (level: number) => void;
   selectedClass: FrosthavenClass<Card> | undefined;
-  selectClass: (playerClass: FrosthavenClass<Card>) => void;
   cards: Card[];
-  selectCards: (cards: Card[]) => void;
   states: Card[][];
   currentStateIndex: number;
+  currentForm: GeminateForm;
+}
+
+type AbilityCardsActions = {
+  setLevel: (level: number) => void;
+  selectClass: (playerClass: FrosthavenClass<Card>) => void;
+  selectCards: (cards: Card[]) => void;
   setStateIndex: (index: number) => void;
   updateStates: <X extends Card>(states: X[][]) => void;
-  currentForm: GeminateForm;
   setForm: (form: GeminateForm) => void;
 }
 
-export const useFrosthavenStore = create<AbilityCardsState>()(
+const initialState: AbilityCardsState = {
+  level: 1,
+  selectedClass: undefined,
+  cards: [],
+  states: [[]],
+  currentStateIndex: 0,
+  currentForm: GeminateForm.melee,
+}
+
+export const useFrosthavenStore = create<AbilityCardsState & AbilityCardsActions>()(
   persist(
     (set, get) => ({
-      level: 1,
+      ...initialState,
       setLevel: (level: number) => set({ level }),
-      selectedClass: undefined,
       selectClass: (playerClass: FrosthavenClass<Card>) => {
         if (playerClass.name !== get().selectedClass?.name) {
           set({ level: 1, selectedClass: playerClass, cards: [], states: [], currentStateIndex: 0 });
         }
       },
-      cards: [],
       selectCards: (cards: Card[]) => set({ cards, states: [cards], currentStateIndex: 0 }),
-      states: [[]],
       updateStates: <X extends Card>(states: X[][]) => set({ states, currentStateIndex: states.length - 1 }),
-      currentStateIndex: 0,
       setStateIndex: (index: number) => set({ currentStateIndex: index }),
-      currentForm: GeminateForm.melee,
       setForm: (currentForm: GeminateForm) => set({ currentForm }),
     }),
     {
