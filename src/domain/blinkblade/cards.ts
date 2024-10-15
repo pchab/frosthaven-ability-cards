e085b280988196d298e7098e633bdebaf1b4e360
Pierre Chabardes <pierre@chabardes.net>
2024-10-15 14:42:47 +0200

    feat(class): add all enhancement slots for blinkblade

--- src/domain/blinkblade/cards.ts
+++ src/domain/blinkblade/cards.ts
@@ -5,7 +5,9 @@ const blinbladePath = '/blinkblade/abilities';
 
 export const blinkbladeCards: Card[] = createCards([
   {
-    level: 'X', name: 'borrowed time', actions: { top: CardActions.activeLost, bottom: CardActions.activeDiscard },
+    level: 'X',
+    name: 'borrowed time',
+    actions: { top: CardActions.activeLost, bottom: CardActions.activeDiscard },
     slots: [
       { x: 43, y: 59 },
       { x: 72, y: 59 },
@@ -15,13 +17,47 @@ export const blinkbladeCards: Card[] = createCards([
       { x: 88, y: 83 },
     ],
   },
-  { level: 'X', name: 'experimental adjustment', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 'X', name: 'sand in the hourglass', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 1, name: 'blurry jab', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 1, name: 'cascading reaction', actions: { top: CardActions.lose, bottom: CardActions.discard } },
-  { level: 1, name: 'drive recharge', actions: { top: CardActions.lose, bottom: CardActions.discard } },
   {
-    level: 1, name: 'hit and run', actions: { top: CardActions.discard, bottom: CardActions.activeLost },
+    level: 'X',
+    name: 'experimental adjustment',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+  },
+  {
+    level: 'X',
+    name: 'sand in the hourglass',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 56, y: 124 } },
+    ],
+  },
+  {
+    level: 1,
+    name: 'blurry jab',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'diamond', position: { x: 84, y: 40 } },
+      { type: 'circle', position: { x: 84, y: 134 } },
+    ],
+  },
+  {
+    level: 1,
+    name: 'cascading reaction',
+    actions: { top: CardActions.lose, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 29 } },
+      { type: 'circle', position: { x: 84, y: 54 } },
+      { type: 'circle', position: { x: 84, y: 78 } },
+    ],
+  },
+  {
+    level: 1,
+    name: 'drive recharge',
+    actions: { top: CardActions.lose, bottom: CardActions.discard },
+  },
+  {
+    level: 1,
+    name: 'hit and run',
+    actions: { top: CardActions.discard, bottom: CardActions.activeLost },
     slots: [
       { x: 39, y: 149 },
       { x: 68, y: 149 },
@@ -29,49 +65,199 @@ export const blinkbladeCards: Card[] = createCards([
       { x: 54, y: 174 },
       { x: 84, y: 174 },
     ],
+    availableEnhancements: [
+      { type: 'circle', position: { x: 68, y: 38 } },
+    ],
   },
-  { level: 1, name: 'kinetic transfer', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 1, name: 'overdrive', actions: { top: CardActions.activeLost, bottom: CardActions.discard } },
-  { level: 1, name: 'power leak', actions: { top: CardActions.discard, bottom: CardActions.lose } },
-  { level: 1, name: 'sap speed', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 1, name: 'temporal displacement', actions: { top: CardActions.discard, bottom: CardActions.discard } },
   {
-    level: 1, name: 'twin strike', actions: { top: CardActions.discard, bottom: CardActions.discard },
+    level: 1,
+    name: 'kinetic transfer',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 30 } },
+      { type: 'diamond', position: { x: 84, y: 56 } },
+      { type: 'circle', position: { x: 84, y: 125 } },
+    ],
+  },
+  {
+    level: 1,
+    name: 'overdrive',
+    actions: { top: CardActions.activeLost, bottom: CardActions.discard },
+  },
+  {
+    level: 1,
+    name: 'power leak',
+    actions: { top: CardActions.discard, bottom: CardActions.lose },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 126 } },
+    ],
+  },
+  {
+    level: 1,
+    name: 'sap speed',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+  },
+  {
+    level: 1,
+    name: 'temporal displacement',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 146 } },
+    ],
+  },
+  {
+    level: 1,
+    name: 'twin strike',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
     availableEnhancements: [
       { type: 'square', position: { x: 84, y: 29 } },
       { type: 'diamond', position: { x: 84, y: 78 } },
       { type: 'circle', position: { x: 84, y: 125 } },
     ],
   },
-  { level: 2, name: 'reckless augmentation', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 2, name: 'systems reboot', actions: { top: CardActions.lose, bottom: CardActions.discard } },
   {
-    level: 3, name: 'double time', actions: { top: CardActions.discard, bottom: CardActions.activeLost },
+    level: 2,
+    name: 'reckless augmentation',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'diamond', position: { x: 84, y: 39 } },
+      { type: 'circle', position: { x: 84, y: 123 } },
+    ],
+  },
+  {
+    level: 2,
+    name: 'systems reboot',
+    actions: { top: CardActions.lose, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'diamond', position: { x: 66, y: 39 } },
+    ],
+  },
+  {
+    level: 3,
+    name: 'double time',
+    actions: { top: CardActions.discard, bottom: CardActions.activeLost },
     slots: [
       { x: 54, y: 149 },
       { x: 84, y: 149 },
       { x: 70, y: 174 },
     ],
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 29 } },
+      { type: 'diamond', position: { x: 84, y: 52 } },
+    ],
   },
-  { level: 3, name: 'make it count', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 4, name: 'potential energy', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 4, name: 'precision timing', actions: { top: CardActions.activeLost, bottom: CardActions.discard } },
   {
-    level: 5, name: 'breakneck speed', actions: { top: CardActions.activeLost, bottom: CardActions.discard },
+    level: 3,
+    name: 'make it count',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 58, y: 29 } },
+      { type: 'circle', position: { x: 84, y: 160 } },
+    ],
+  },
+  {
+    level: 4,
+    name: 'potential energy',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'diamond', position: { x: 90, y: 46 } },
+      { type: 'circle', position: { x: 90, y: 140 } },
+    ],
+  },
+  {
+    level: 4,
+    name: 'precision timing',
+    actions: { top: CardActions.activeLost, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 125 } },
+      { type: 'circle', position: { x: 84, y: 175 } },
+    ],
+  },
+  {
+    level: 5,
+    name: 'breakneck speed',
+    actions: { top: CardActions.activeLost, bottom: CardActions.discard },
     slots: [
       { x: 23, y: 78 },
       { x: 53, y: 78 },
       { x: 83, y: 78 },
       { x: 113, y: 78 },
     ],
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 132 } },
+    ],
   },
-  { level: 5, name: 'flashing flurry', actions: { top: CardActions.discard, bottom: CardActions.lose } },
-  { level: 6, name: 'stab them all', actions: { top: CardActions.discard, bottom: CardActions.activeDiscard } },
-  { level: 6, name: 'the knifes edge', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 7, name: 'phasing blades', actions: { top: CardActions.activeLost, bottom: CardActions.discard } },
-  { level: 7, name: 'rushed to the end', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 8, name: 'fastest alive', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 8, name: 'quantum uncertainty', actions: { top: CardActions.discard, bottom: CardActions.discard } },
-  { level: 9, name: 'fractured timeline', actions: { top: CardActions.activeLost, bottom: CardActions.discard } },
-  { level: 9, name: 'reverse the flow', actions: { top: CardActions.activeDiscard, bottom: CardActions.lose } },
+  {
+    level: 5,
+    name: 'flashing flurry',
+    actions: { top: CardActions.discard, bottom: CardActions.lose },
+    availableEnhancements: [
+      { type: 'diamond', position: { x: 55, y: 135 } },
+    ],
+  },
+  {
+    level: 6,
+    name: 'stab them all',
+    actions: { top: CardActions.discard, bottom: CardActions.activeDiscard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 54 } },
+      { type: 'diamond', position: { x: 84, y: 85 } },
+    ],
+  },
+  {
+    level: 6,
+    name: 'the knifes edge',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'square', position: { x: 66, y: 132 } },
+    ],
+  },
+  {
+    level: 7,
+    name: 'phasing blades',
+    actions: { top: CardActions.activeLost, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 136 } },
+    ],
+  },
+  {
+    level: 7,
+    name: 'rushed to the end',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'diamond', position: { x: 84, y: 46 } },
+      { type: 'circle', position: { x: 84, y: 139 } },
+      { type: 'circle', position: { x: 84, y: 171 } },
+    ],
+  },
+  {
+    level: 8,
+    name: 'fastest alive',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 146 } },
+    ],
+  },
+  {
+    level: 8,
+    name: 'quantum uncertainty',
+    actions: { top: CardActions.discard, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'diamond', position: { x: 84, y: 34 } },
+      { type: 'circle', position: { x: 84, y: 136 } },
+    ],
+  },
+  {
+    level: 9,
+    name: 'fractured timeline',
+    actions: { top: CardActions.activeLost, bottom: CardActions.discard },
+    availableEnhancements: [
+      { type: 'circle', position: { x: 84, y: 129 } },
+    ],
+  },
+  {
+    level: 9,
+    name: 'reverse the flow',
+    actions: { top: CardActions.activeDiscard, bottom: CardActions.lose },
+  },
 ], blinbladePath);
