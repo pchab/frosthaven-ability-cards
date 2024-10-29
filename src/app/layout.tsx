2436d8f581d44b40410ee86f31bd177d19b46b47
Pierre Chabardes <pierre@chabardes.net>
2024-10-29 12:56:55 +0100

    feat(next): readd speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           backgroundAttachment: 'fixed',
         }}
       >
+        <SpeedInsights />
         <main>
           {children}
         </main>
