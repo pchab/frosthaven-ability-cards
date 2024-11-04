71ecfac976dfbec98b3d54c347105df8c2eb4eb1
Pierre Chabardes <pierre@chabardes.net>
2024-11-04 16:24:52 +0100

    feat(menu): add menu animation

--- src/app/MenuButton.tsx
+++ src/app/MenuButton.tsx
@@ -3,17 +3,31 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function MenuButton() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   return <div className='absolute right-0 top-0 p-4 flex flex-col items-end gap-2'>
-    <button className='rounded-full' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+    <motion.button
+      className='rounded-full'
+      onClick={() => setIsMenuOpen(!isMenuOpen)}
+      whileTap={{ rotate: 360 }}
+      transition={{ duration: 0.3 }}
+    >
       <Image src='/icons/cogwheel.svg' alt='Settings' width={24} height={24} />
-    </button>
-    {isMenuOpen && <div className='bg-black border-solid rounded-lg border-2 p-4'>
-      <Link type='submit' href='https://github.com/pchab/frosthaven-ability-cards/issues/new' target='_blank'>
-        Report Issue
-      </Link>
-    </div>}
+    </motion.button>
+    <AnimatePresence>
+      {isMenuOpen && <motion.div
+        className='bg-black border-solid rounded-lg border-2 p-4'
+        initial={{ x: '100%' }}
+        animate={{ x: '0%' }}
+        exit={{ x: '100%' }}
+        transition={{ duration: 0.3 }}
+      >
+        <Link type='submit' href='https://github.com/pchab/frosthaven-ability-cards/issues/new' target='_blank'>
+          Report Issue
+        </Link>
+      </motion.div>}
+    </AnimatePresence>
   </div>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         }}
       >
         <SpeedInsights />
-        <main>
+        <main role='main'>
           <MenuButton />
           {children}
         </main>
