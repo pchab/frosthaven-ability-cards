9f9cea5862943f525c12ac1054054481d6bf7006
Pierre Chabardes <pierre@chabardes.net>
2024-11-07 14:39:52 +0100

    feat(ws): add game state in localstorage

---
+++ src/stores/game.store.ts
@@ -0,0 +1,16 @@
+import type { GameState } from '@/domain/secretariat/game.state';
+
+export function getGameState() {
+  if (!window.localStorage) {
+    return undefined;
+  }
+  const gameStateString = localStorage.getItem('game');
+  return JSON.parse(gameStateString ?? '{}') as GameState;
+}
+
+export function setGameState(state: GameState) {
+  if (!window.localStorage) {
+    return undefined;
+  }
+  localStorage.setItem('game', JSON.stringify(state));
+}
