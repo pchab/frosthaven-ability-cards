b37ef1de6f6b3887dcfde0018006ae558f166fc1
Pierre Chabardes <pierre@chabardes.net>
2024-11-07 16:19:01 +0100

    feat(class): add banner spear initiatives

--- src/domain/banner-spear/cards.ts
+++ src/domain/banner-spear/cards.ts
@@ -12,6 +12,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamond', position: { x: 56, y: 54 } },
       { type: 'summonSquare', position: { x: 65, y: 144 } },
     ],
+    initiative: 18,
   },
   {
     level: 'X',
@@ -22,11 +23,13 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'summonSquare', position: { x: 68, y: 144 } },
       { type: 'summonSquare', position: { x: 68, y: 159 } },
     ],
+    initiative: 22,
   },
   {
     level: 'X',
     name: 'resolved courage',
-    actions: { top: CardActions.discard, bottom: CardActions.activeDiscard }
+    actions: { top: CardActions.discard, bottom: CardActions.activeDiscard },
+    initiative: 10,
   },
   {
     level: 1,
@@ -36,6 +39,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'summonSquare', position: { x: 70, y: 140 } },
       { type: 'summonSquare', position: { x: 115, y: 140 } },
     ],
+    initiative: 60,
   },
   {
     level: 1,
@@ -46,6 +50,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'circle', position: { x: 91, y: 137 } },
       { type: 'square', position: { x: 91, y: 171 } },
     ],
+    initiative: 32,
   },
   {
     level: 1,
@@ -55,6 +60,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'circle', position: { x: 91, y: 35 } },
       { type: 'circle', position: { x: 91, y: 141 } },
     ],
+    initiative: 15,
   },
   {
     level: 1,
@@ -64,6 +70,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamond', position: { x: 71, y: 53 } },
       { type: 'circle', position: { x: 88, y: 141 } },
     ],
+    initiative: 21,
   },
   {
     level: 1,
@@ -73,6 +80,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamond', position: { x: 52, y: 62 } },
       { type: 'circle', position: { x: 89, y: 141 } },
     ],
+    initiative: 69,
   },
   {
     level: 1,
@@ -82,6 +90,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'hex', position: { x: 109, y: 44, size: 15 } },
       { type: 'summonSquare', position: { x: 65, y: 143 } },
     ],
+    initiative: 71,
   },
   {
     level: 1,
@@ -91,11 +100,13 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamondPlus', position: { x: 64, y: 60 } },
       { type: 'square', position: { x: 90, y: 159 } },
     ],
+    initiative: 25,
   },
   {
     level: 1,
     name: 'set for the charge',
-    actions: { top: CardActions.activeDiscard, bottom: CardActions.discard }
+    actions: { top: CardActions.activeDiscard, bottom: CardActions.discard },
+    initiative: 6,
   },
   {
     level: 1,
@@ -105,11 +116,13 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'hex', position: { x: 113, y: 31, size: 15 } },
       { type: 'circle', position: { x: 90, y: 154 } },
     ],
+    initiative: 67,
   },
   {
     level: 1,
     name: 'unbreakable wall',
-    actions: { top: CardActions.discard, bottom: CardActions.activeLost }
+    actions: { top: CardActions.discard, bottom: CardActions.activeLost },
+    initiative: 83,
   },
   {
     level: 2,
@@ -119,6 +132,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamond', position: { x: 53, y: 59 } },
       { type: 'circle', position: { x: 63, y: 167 } },
     ],
+    initiative: 62,
   },
   {
     level: 2,
@@ -128,6 +142,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamond', position: { x: 71, y: 40 } },
       { type: 'square', position: { x: 90, y: 160 } },
     ],
+    initiative: 17,
   },
   {
     level: 3,
@@ -137,6 +152,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamond', position: { x: 52, y: 58 } },
       { type: 'circle', position: { x: 90, y: 135 } },
     ],
+    initiative: 87,
   },
   {
     level: 3,
@@ -145,6 +161,7 @@ export const bannerSpearCards: Card[] = createCards([
     availableEnhancements: [
       { type: 'summonSquare', position: { x: 64, y: 145 } },
     ],
+    initiative: 24,
   },
   {
     level: 4,
@@ -156,6 +173,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'summonSquare', position: { x: 98, y: 144 } },
       { type: 'summonSquare', position: { x: 64, y: 159 } },
     ],
+    initiative: 20,
   },
   {
     level: 4,
@@ -165,11 +183,13 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'circle', position: { x: 90, y: 137 } },
       { type: 'square', position: { x: 90, y: 170 } },
     ],
+    initiative: 72,
   },
   {
     level: 5,
     name: 'explosive epicenter',
     actions: { top: CardActions.discard, bottom: CardActions.activeLost },
+    initiative: 78,
   },
   {
     level: 5,
@@ -180,12 +200,14 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'circle', position: { x: 89, y: 129 } },
       { type: 'circle', position: { x: 89, y: 169 } },
     ],
+    initiative: 5,
   },
   {
     level: 6, name: 'barricade', actions: { top: CardActions.activeDiscard, bottom: CardActions.activeLost },
     availableEnhancements: [
       { type: 'summonSquare', position: { x: 65, y: 144 } },
     ],
+    initiative: 16,
   },
   {
     level: 6, name: 'bolstering shout', actions: { top: CardActions.discard, bottom: CardActions.discard },
@@ -194,6 +216,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'circle', position: { x: 89, y: 142 } },
       { type: 'square', position: { x: 81, y: 163 } },
     ],
+    initiative: 75,
   },
   {
     level: 7, name: 'lead from afar', actions: { top: CardActions.discard, bottom: CardActions.activeLost },
@@ -203,6 +226,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'summonSquare', position: { x: 97, y: 145 } },
       { type: 'summonSquare', position: { x: 97, y: 155 } },
     ],
+    initiative: 80,
   },
   {
     level: 7, name: 'tri thrust', actions: { top: CardActions.discard, bottom: CardActions.discard },
@@ -211,6 +235,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'circle', position: { x: 91, y: 131 } },
       { type: 'square', position: { x: 72, y: 153 } },
     ],
+    initiative: 27,
   },
   {
     level: 8, name: 'sweeping aid', actions: { top: CardActions.discard, bottom: CardActions.discard },
@@ -218,6 +243,7 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'diamond', position: { x: 63, y: 37 } },
       { type: 'circle', position: { x: 89, y: 154 } },
     ],
+    initiative: 73,
   },
   {
     level: 8, name: 'taunting howl', actions: { top: CardActions.activeLost, bottom: CardActions.discard },
@@ -226,17 +252,20 @@ export const bannerSpearCards: Card[] = createCards([
       { type: 'circle', position: { x: 89, y: 135 } },
       { type: 'square', position: { x: 89, y: 170 } },
     ],
+    initiative: 11,
   },
   {
     level: 9, name: 'hail of spears', actions: { top: CardActions.discard, bottom: CardActions.lose },
     availableEnhancements: [
       { type: 'diamond', position: { x: 71, y: 37 } },
     ],
+    initiative: 44,
   },
   {
     level: 9, name: 'take no prisoners', actions: { top: CardActions.discard, bottom: CardActions.activeLost },
     availableEnhancements: [
       { type: 'summonSquare', position: { x: 64, y: 145 } },
     ],
+    initiative: 85,
   },
 ], bannerSpearPath);
