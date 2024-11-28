205c9591d95b995faf76acd28e4ecca9eb8ef173
Pierre Chabardes <pierre@chabardes.net>
2024-11-28 11:46:47 +0100

    feat(store): update indexedDb version to load new class

--- src/stores/indexed-db.ts
+++ src/stores/indexed-db.ts
@@ -12,7 +12,7 @@ const db = (function () {
       }
 
       return new Promise<IDBDatabase>((resolve) => {
-        const request = indexedDB.open(STORE_NAME, 2);
+        const request = indexedDB.open(STORE_NAME, 3);
 
         request.onsuccess = () => {
           resolve(request.result);
