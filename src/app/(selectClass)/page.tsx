509051268738e2549d225acf586b62ab15b6eba9
Pierre Chabardes <pierre@chabardes.net>
2024-12-05 13:53:58 +0100

    feat(selectClass): largest content is no longer lazy load

--- src/app/(selectClass)/page.tsx
+++ src/app/(selectClass)/page.tsx
@@ -1,11 +1,11 @@
+import Heading from '@/app/_components/layout/Heading';
+import { frosthavenClasses } from '@/domain/frosthaven-class';
 import Image from 'next/image';
 import SelectClass from './SelectClass';
-import Heading from '../_components/layout/Heading';
-import { frosthavenClasses } from '@/domain/frosthaven-class';
 
 export default function SelectClassPage() {
   return <div className='flex flex-col gap-16 p-16 place-items-center'>
-    <Image src='/fh-frosthaven-logo.webp' alt='Frosthaven logo' width={600} height={87} />
+    <Image loading='eager' src='/fh-frosthaven-logo.webp' alt='Frosthaven logo' width={600} height={87} />
     <Heading title='Select your class' />
     <div className='grid grid-cols-3 lg:grid-cols-6 gap-8 p-6 place-items-center bg-linear-to-b from-black to-blue-500 rounded-lg'>
       {frosthavenClasses.map((fhClass) => <SelectClass key={fhClass.name} fhClass={fhClass} />)}
