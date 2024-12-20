6b73e480fde6037d1a7ebffca1975e33f05f66a5
Pierre Chabardes <pierre@chabardes.net>
2024-12-20 10:14:53 +0100

    feat(sitemap): add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { Card } from '@/domain/cards.type';
+import { classNameToURI, frosthavenClasses } from '@/domain/frosthaven-class';
+import type { FrosthavenClass } from '@/domain/frosthaven-class.type';
+import type { MetadataRoute } from 'next';
+
+const prodUrl = 'https://frosthaven-cards.vercel.app';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const fhClassIcons = frosthavenClasses.map(({ path }) => `${prodUrl}${path}`);
+  const selectClassUrl = {
+    url: prodUrl,
+    images: fhClassIcons,
+  };
+
+  const fhClassCardsUrls = (cards: Card[]) => cards
+    .map(({ path }) => `${prodUrl}${path}`);
+  const selectCardsUrls = frosthavenClasses.map(({ name, cards }) => ({
+    url: `${prodUrl}/${classNameToURI(name)}/select`,
+    images: fhClassCardsUrls(cards),
+  }));
+  const playCardsUrls = frosthavenClasses.map(({ name, cards }) => ({
+    url: `${prodUrl}/${classNameToURI(name)}/play`,
+    images: fhClassCardsUrls(cards),
+  }));
+  return [
+    selectClassUrl,
+    ...selectCardsUrls,
+    ...playCardsUrls,
+  ];
+}
