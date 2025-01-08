76d2b8fad5bfca80a029564363a51179baa44902
Pierre Chabardes <pierre@chabardes.net>
2025-01-08 20:53:31 +0100

    update indexed db version

--- src/stores/indexed-db.ts
+++ src/stores/indexed-db.ts
@@ -13,7 +13,7 @@ const db = (function () {
       }
 
       return new Promise<IDBDatabase>((resolve) => {
-        const request = indexedDB.open(STORE_NAME, 10);
+        const request = indexedDB.open(STORE_NAME, 11);
 
         request.onsuccess = () => {
           resolve(request.result);
