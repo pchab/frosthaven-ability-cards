0577801facaeff6af81962f046ea58108d146002
Pierre Chabardes <pierre@chabardes.net>
2025-04-10 13:27:36 +0200

    feat(UX): use header instead of aria role

--- src/app/(selectClass)/page.tsx
+++ src/app/(selectClass)/page.tsx
@@ -5,7 +5,9 @@ import SelectClass from './SelectClass';
 
 export default function SelectClassPage() {
   return <div className='flex flex-col gap-16 p-16 place-items-center'>
-    <Image role='banner' priority loading='eager' src='/fh-frosthaven-logo.webp' alt='Frosthaven logo' width={600} height={87} />
+    <header>
+      <Image priority loading='eager' src='/fh-frosthaven-logo.webp' alt='Frosthaven logo' width={600} height={87} />
+    </header>
     <Heading title='Select your class' />
     <div className='grid grid-cols-3 lg:grid-cols-6 gap-8 p-6 place-items-center bg-linear-to-b from-black to-blue-500 rounded-lg'>
       {frosthavenClasses.map((fhClass) => <SelectClass key={fhClass.name} fhClass={fhClass} />)}
