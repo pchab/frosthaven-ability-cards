b9c6124c123dfcb6052c54796fff0b1f566494e7
Pierre Chabardes <pierre@chabardes.net>
2025-04-29 17:03:17 +0200

    fix(UX): prevent elements height above the board area to affect each others

--- src/app/_components/layout/BoardArea.tsx
+++ src/app/_components/layout/BoardArea.tsx
@@ -13,7 +13,7 @@ export default function BoardArea({
     <div className={`p-2 border-r-2 border-b-2 border-t-2 border-primary ${!actions && 'rounded-tr-lg'} rounded-b-lg bg-linear-to-r from-slate-700 to-blue-300`}>
       {children}
     </div>
-    <div className='flex justify-between gap-2'>
+    <div className='flex justify-between items-end gap-2'>
       <h2 className='text-lg px-2 border-t-2 border-r-2 rounded-t-lg bg-linear-to-r from-slate-700 to-slate-500 border-primary -mb-[2px] pb-[2px]'>{title}</h2>
       {actions &&
         <div className='flex gap-1'>
